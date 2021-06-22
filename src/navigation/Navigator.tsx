3b3329dca8d59a6af344f2f30625f6264d75049e
Daniel <dunapanta2@gmail.com>
2021-06-22 14:53:05 -0500

    Boilerplate with icons and navigation

---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen name="Home" component={Home} /> */}
+    </Stack.Navigator>
+  );
+};
+
+export default Navigator;
