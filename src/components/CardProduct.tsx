b75e44878bbacc06409568bae01399d123d31eed
Daniel <dunapanta2@gmail.com>
2021-06-29 16:31:48 -0500

    Product Card

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
 import {StyleSheet, View, Text, useWindowDimensions, Image} from 'react-native';
+import {color} from 'react-native-reanimated';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import Colors from '../constants/Colors';
 import {Producto} from '../interfaces/appInterfaces';
 
@@ -13,6 +15,14 @@ export const CardProduct = ({product, index}: Props) => {
   const {width, height} = useWindowDimensions();
   return (
     <View style={{...styles.card, width: width / 1.9, height: height / 2.8}}>
+      <View
+        style={{
+          ...styles.cardOverlay,
+          width: width / 1.9,
+          height: height / 2.8,
+          opacity: 0
+        }}
+      />
       <View style={{...styles.priceTag, height: height / 16, width: width / 5}}>
         <Text
           style={{
@@ -32,7 +42,42 @@ export const CardProduct = ({product, index}: Props) => {
           style={{...styles.cardImg, height: height / 3.8}}
         />
       )}
-      <View style={{...styles.cardDetails, height: height / 8}}></View>
+      <View style={{...styles.cardDetails, height: height / 8}}>
+        <View style={styles.cardHeader}>
+          <View>
+            <Text style={styles.productName} numberOfLines={1}>
+              {product.nombre.length <= 13
+                ? product.nombre
+                : product.nombre.slice(0, 12) + '...'}
+            </Text>
+            <Text
+              style={{...styles.categoryName, marginTop: height / 200}}
+              numberOfLines={1}>
+              {product.categoria.nombre.length <= 20
+                ? product.categoria.nombre
+                : product.categoria.nombre.slice(0, 19) + '...'}
+            </Text>
+          </View>
+          <Icon name="star-outline" size={26} color={Colors.primary} />
+        </View>
+        <View style={{flexDirection: 'row', marginTop: height / 200}}>
+          <Text
+            style={{
+              fontSize: 13,
+              fontWeight: 'bold',
+              color: Colors.secondaryDark,
+            }}>
+            Publicado por:{' '}
+          </Text>
+          <Text
+            style={{fontSize: 13, color: Colors.secondaryDark}}
+            numberOfLines={1}>
+            {product.usuario.nombre.length < 11
+              ? product.usuario.nombre
+              : product.usuario.nombre.slice(0, 10) + '...'}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -78,7 +123,26 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     position: 'absolute',
     bottom: 0,
-    paddingLeft: 20,
+    padding: 14,
     width: '100%',
+  },
+  cardHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  productName: {
+    color: Colors.secondaryDark,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  categoryName: {
+    color: Colors.secondary,
+    fontSize: 11,
+  },
+  cardOverlay: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    zIndex: 1000,
+    borderRadius: 15,
   },
 });
