a8a99a5abcf7c857b2acd6fedafe3328f73a2a01
Daniel <dunapanta@outlook.es>
2021-07-03 12:16:06 -0500

    Last Products flatList

--- src/components/LastProductCard.tsx
+++ src/components/LastProductCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Image, StyleSheet, Text} from 'react-native';
+import Colors from '../constants/Colors';
 
 import {Producto} from '../interfaces/appInterfaces';
 
@@ -10,7 +11,27 @@ interface Props {
 export const LastProductCard = ({product}: Props) => {
   return (
     <View style={styles.card}>
-      <Text>LastProductCard</Text>
+      {product.img ? (
+        <Image source={{uri: product.img}} style={styles.image} />
+      ) : (
+        <Image source={require('../assets/noImage.png')} style={styles.image} />
+      )}
+      <View style={styles.textContainer}>
+        {product.nombre.length < 16 ? (
+          <Text style={styles.text}>{product.nombre}</Text>
+        ) : (
+          <Text style={styles.text}>{product.nombre.substr(0, 15) + '..'}</Text>
+        )}
+
+        {/* Categoria */}
+        {product.categoria.nombre.length < 18 ? (
+          <Text style={styles.smallText}>{product.categoria.nombre}</Text>
+        ) : (
+          <Text style={styles.smallText}>
+            {product.categoria.nombre.substr(0, 20) + '..'}
+          </Text>
+        )}
+      </View>
     </View>
   );
 };
@@ -32,5 +53,26 @@ const styles = StyleSheet.create({
     shadowRadius: 4.65,
 
     elevation: 15,
+  },
+  image: {
+    height: 80,
+    width: '100%',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  textContainer: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  text: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    color: Colors.secondaryDark,
+  },
+  smallText: {
+    fontSize: 8,
+    paddingTop: 2,
+    fontWeight: 'bold',
+    color: Colors.secondary,
   },
 });
