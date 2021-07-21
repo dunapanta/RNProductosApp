0ceec880deef300f26e37bcf0ed580b03f7a4a4c
Daniel <dunapanta@outlook.es>
2021-07-20 19:51:52 -0500

    Adding interfaces for products search

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {useEffect} from 'react';
 import {useState} from 'react';
 import productosApi from '../api/productosApi';
-import {Producto} from '../interfaces/appInterfaces';
+import {
+  Producto,
+  SearchResponse,
+  SearchResult,
+} from '../interfaces/appInterfaces';
 
 export const useSearch = () => {
-  const [searchProducts, setSearchProducts] = useState<Producto[]>([]);
+  const [searchProducts, setSearchProducts] = useState<SearchResult[]>([]);
   const [loading, setLoading] = useState(false);
   const [term, setTerm] = useState('');
 
@@ -18,8 +22,10 @@ export const useSearch = () => {
 
   const searchingProducts = async () => {
     setLoading(true);
-    const resp = await productosApi.get(`/buscar/productos/${term}`);
-    setSearchProducts(resp.data);
+    const resp = await productosApi.get<SearchResponse>(
+      `/buscar/productos/${term}`,
+    );
+    setSearchProducts(resp.data.results);
     //console.log('Data', resp.data);
     setLoading(false);
   };
